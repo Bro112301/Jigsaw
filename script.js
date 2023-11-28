77290de20a5c74b0e4db20312ba72d2932296899
Bro112301 <142183375+Bro112301@users.noreply.github.com>
2023-11-28 10:05:26 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,36 @@
 const puzzleContainer = document.getElementById('puzzle-container');
 
-// Number of rows and columns in the puzzle
-const numRows = 3;
-const numCols = 3;
-
 // Function to create puzzle pieces
 function createPuzzle() {
-    const pieceWidth = puzzleContainer.offsetWidth / numCols;
-    const pieceHeight = puzzleContainer.offsetHeight / numRows;
+    // ... (same as before)
+}
 
-    for (let i = 0; i < numRows; i++) {
-        for (let j = 0; j < numCols; j++) {
-            const piece = document.createElement('div');
-            piece.classList.add('puzzle-piece');
-            piece.style.width = `${pieceWidth}px`;
-            piece.style.height = `${pieceHeight}px`;
-            piece.style.backgroundImage = `url('your_image.jpg')`;
-            piece.style.backgroundSize = `${numCols * 100}% ${numRows * 100}%`;
-            piece.style.backgroundPosition = `-${j * (100 / (numCols - 1))}% -${i * (100 / (numRows - 1))}%`;
-            piece.draggable = true;
-
-            puzzleContainer.appendChild(piece);
-        }
-    }
-
-    // Add drag and drop event listeners
-    puzzleContainer.addEventListener('dragstart', (e) => {
-        e.dataTransfer.setData('text/plain', '');
+// Function to shuffle puzzle pieces
+function shufflePuzzle() {
+    const pieces = document.querySelectorAll('.puzzle-piece');
+    pieces.forEach(piece => {
+        const randomX = Math.floor(Math.random() * numCols);
+        const randomY = Math.floor(Math.random() * numRows);
+        piece.style.order = randomY * numCols + randomX;
     });
+}
 
-    puzzleContainer.addEventListener('dragover', (e) => {
-        e.preventDefault();
+// Function to reset puzzle pieces
+function resetPuzzle() {
+    const pieces = document.querySelectorAll('.puzzle-piece');
+    pieces.forEach((piece, index) => {
+        piece.style.order = index;
     });
+}
 
-    puzzleContainer.addEventListener('drop', (e) => {
-        e.preventDefault();
-        const draggedPiece = document.querySelector('.dragging');
-        const target = e.target;
-
-        if (target.classList.contains('puzzle-piece')) {
-            puzzleContainer.insertBefore(draggedPiece, target);
-        } else {
-            puzzleContainer.appendChild(draggedPiece);
-        }
-
-        draggedPiece.classList.remove('dragging');
-    });
+// Function to save puzzle state
+function savePuzzle() {
+    const pieces = document.querySelectorAll('.puzzle-piece');
+    const puzzleState = Array.from(pieces).map(piece => piece.style.order);
+    const puzzleLink = `${window.location.origin}?state=${puzzleState.join(',')}`;
+    
+    // You can use the puzzleLink as a shareable link or save it in your backend
+    alert(`Puzzle saved! Share this link: ${puzzleLink}`);
 }
 
 // Initialize puzzle
