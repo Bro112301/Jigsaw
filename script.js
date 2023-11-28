a6336c40f2589f8f0b1c6c52d47a175f3a934d5f
Bro112301 <142183375+Bro112301@users.noreply.github.com>
2023-11-28 10:28:51 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,36 @@
 const puzzleContainer = document.getElementById('puzzle-container');
+let numRows = 3;
+let numCols = 3;
+
+// Function to load user-uploaded image
+function loadImage() {
+    const imageInput = document.getElementById('imageInput');
+    const file = imageInput.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            puzzleContainer.style.backgroundImage = `url('${e.target.result}')`;
+        };
+        reader.readAsDataURL(file);
+    }
+}
+
+// Function to create custom puzzle
+function createCustomPuzzle() {
+    numRows = parseInt(document.getElementById('rows').value);
+    numCols = parseInt(document.getElementById('cols').value);
+
+    puzzleContainer.innerHTML = ''; // Clear existing puzzle
+    puzzleContainer.style.gridTemplateColumns = `repeat(${numCols}, 1fr)`;
+    puzzleContainer.style.gridTemplateRows = `repeat(${numRows}, 1fr)`;
+
+    createPuzzle();
+}
 
 // Function to create puzzle pieces
 function createPuzzle() {
     // ... (same as before)
 }
 
-// Function to shuffle puzzle pieces
-function shufflePuzzle() {
-    const pieces = document.querySelectorAll('.puzzle-piece');
-    pieces.forEach(piece => {
-        const randomX = Math.floor(Math.random() * numCols);
-        const randomY = Math.floor(Math.random() * numRows);
-        piece.style.order = randomY * numCols + randomX;
-    });
-}
-
-// Function to reset puzzle pieces
-function resetPuzzle() {
-    const pieces = document.querySelectorAll('.puzzle-piece');
-    pieces.forEach((piece, index) => {
-        piece.style.order = index;
-    });
-}
-
-// Function to save puzzle state
-function savePuzzle() {
-    const pieces = document.querySelectorAll('.puzzle-piece');
-    const puzzleState = Array.from(pieces).map(piece => piece.style.order);
-    const puzzleLink = `${window.location.origin}?state=${puzzleState.join(',')}`;
-    
-    // You can use the puzzleLink as a shareable link or save it in your backend
-    alert(`Puzzle saved! Share this link: ${puzzleLink}`);
-}
-
-// Initialize puzzle
-createPuzzle();
+// ... (same as before)
