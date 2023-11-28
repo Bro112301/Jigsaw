6df77173079589e4fde857795757fc6a7ce7e81f
Bro112301 <142183375+Bro112301@users.noreply.github.com>
2023-11-28 11:45:47 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,13 @@
-// Function to create custom puzzle and open a new page
-function createCustomPuzzle() {
-    numRows = parseInt(document.getElementById('rows').value);
-    numCols = parseInt(document.getElementById('cols').value);
+// Function to load user-uploaded image
+function loadImage() {
+    const imageInput = document.getElementById('imageInput');
+    const file = imageInput.files[0];
 
-    const puzzleState = generatePuzzleState();
-    const puzzleLink = `puzzle.html?state=${puzzleState.join(',')}`;
-
-    // Open a new window or tab with the puzzle
-    window.open(puzzleLink, '_blank');
-}
-
-// Function to generate puzzle state
-function generatePuzzleState() {
-    const puzzleState = [];
-    const totalPieces = numRows * numCols;
-
-    for (let i = 0; i < totalPieces; i++) {
-        puzzleState.push(i);
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            puzzleContainer.style.backgroundImage = `url('${e.target.result}')`;
+        };
+        reader.readAsDataURL(file);
     }
-
-    // Shuffle the puzzle state
-    for (let i = totalPieces - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [puzzleState[i], puzzleState[j]] = [puzzleState[j], puzzleState[i]];
-    }
-
-    return puzzleState;
 }
