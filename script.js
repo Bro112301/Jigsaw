74bccf344b3304b31f9832ea980976a89e9d7a5e
Bro112301 <142183375+Bro112301@users.noreply.github.com>
2023-11-28 11:53:35 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-// Function to load user-uploaded image
-function loadImage() {
+// Function to create custom puzzle and open a new page
+function createCustomPuzzle() {
     const imageInput = document.getElementById('imageInput');
     const file = imageInput.files[0];
 
-    if (file) {
-        const reader = new FileReader();
-        reader.onload = function (e) {
-            puzzleContainer.style.backgroundImage = `url('${e.target.result}')`;
-        };
-        reader.readAsDataURL(file);
+    if (!file) {
+        alert("Please choose an image.");
+        return;
     }
+
+    const reader = new FileReader();
+    reader.onload = function (e) {
+        const puzzleState = generatePuzzleState();
+        const puzzleLink = `puzzle.html?state=${puzzleState.join(',')}&image=${encodeURIComponent(e.target.result)}`;
+
+        // Open a new window or tab with the puzzle
+        window.open(puzzleLink, '_blank');
+    };
+
+    reader.readAsDataURL(file);
 }
